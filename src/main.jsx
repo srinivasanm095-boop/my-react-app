b144d94a461cc133a74468f39661dda6856ce946
srinivasanm095-boop <srinivasanm095@gmail.com>
2026-06-17 15:00:35 +0530

    my-react-app

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import JSX from "./Sun.jsx"
+import JSX from "./events.jsx"
 
 
 createRoot(document.getElementById('root')).render(
